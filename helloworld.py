cad774dbcfac61eb3d7956bc6a3d9adc4db47fc8
tuhu <l200220244@student.ums.ac.id>
2024-10-29 14:36:23 +0700

    dua

--- helloworld.py
+++ helloworld.py
@@ -1,57 +1,47 @@
-# helloworld.py
+from metaflow import FlowSpec, step
 
-class Mahasiswa:
-    def __init__(self, nama, nim):
-        self.nama = nama
-        self.nim = nim
-        self.spp_terbayar = False
-        self.mata_kuliah = []
-        self.nilai_akhir = {}
+class KuliahFlow(FlowSpec):
 
-    def bayar_spp(self):
-        print(f"{self.nama} sedang membayar SPP...")
+    @step
+    def start(self):
+        """Mulai Proses: Bayar SPP"""
+        print("Bayar SPP...")
         self.spp_terbayar = True
-        print("SPP berhasil dibayar!")
+        print("SPP telah dibayar!")
+        self.next(self.daftar_mata_kuliah)
 
-    def daftar_mata_kuliah(self, mata_kuliah):
+    @step
+    def daftar_mata_kuliah(self):
+        """Mendaftar Mata Kuliah"""
         if self.spp_terbayar:
-            self.mata_kuliah.append(mata_kuliah)
-            print(f"{self.nama} berhasil mendaftar mata kuliah {mata_kuliah}.")
+            self.mata_kuliah = ["Algoritma", "Struktur Data"]
+            print("Mendaftar mata kuliah:", ", ".join(self.mata_kuliah))
         else:
-            print("SPP belum dibayar. Tidak bisa mendaftar mata kuliah.")
+            print("SPP belum dibayar, tidak bisa mendaftar mata kuliah.")
+        self.next(self.ikut_kuliah)
 
-    def ikuti_perkuliahan(self):
-        if not self.mata_kuliah:
-            print("Belum ada mata kuliah yang diambil.")
-            return
+    @step
+    def ikut_kuliah(self):
+        """Mengikuti Kuliah"""
         for mk in self.mata_kuliah:
-            print(f"{self.nama} mengikuti perkuliahan {mk}.")
+            print(f"Mengikuti perkuliahan: {mk}")
+        self.next(self.ujian)
 
-    def ujian(self, mata_kuliah, nilai):
-        if mata_kuliah in self.mata_kuliah:
-            print(f"{self.nama} mengikuti ujian {mata_kuliah} dengan nilai {nilai}.")
-            self.nilai_akhir[mata_kuliah] = nilai
-        else:
-            print(f"{mata_kuliah} belum terdaftar dalam mata kuliah yang diambil.")
+    @step
+    def ujian(self):
+        """Mengikuti Ujian dan Mendapatkan Nilai"""
+        self.nilai = {"Algoritma": 85, "Struktur Data": 90}
+        for mk, nilai in self.nilai.items():
+            print(f"Ujian {mk} selesai. Nilai: {nilai}")
+        self.next(self.end)
 
-    def tampilkan_nilai_akhir(self):
-        print(f"Nilai akhir {self.nama}:")
-        for mk, nilai in self.nilai_akhir.items():
+    @step
+    def end(self):
+        """Akhir Proses: Menampilkan Nilai Akhir"""
+        print("Nilai akhir:")
+        for mk, nilai in self.nilai.items():
             print(f"{mk}: {nilai}")
+        print("Proses kuliah selesai!")
 
-
-# Proses simulasi
-if __name__ == "__main__":
-    # Buat objek Mahasiswa
-    mahasiswa1 = Mahasiswa("Budi", "123456")
-
-    # Langkah-langkah
-    mahasiswa1.bayar_spp()                        # Bayar SPP
-    mahasiswa1.daftar_mata_kuliah("Algoritma")    # Daftar mata kuliah
-    mahasiswa1.daftar_mata_kuliah("Struktur Data")
-    
-    mahasiswa1.ikuti_perkuliahan()                # Mengikuti kuliah
-    mahasiswa1.ujian("Algoritma", 85)             # Mengikuti ujian dan input nilai
-    mahasiswa1.ujian("Struktur Data", 90)
-    
-    mahasiswa1.tampilkan_nilai_akhir()            # Tampilkan nilai akhir
+if __name__ == '__main__':
+    KuliahFlow()
